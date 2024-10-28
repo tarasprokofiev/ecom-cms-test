ed00758f1b92b4cd451187cddd0bf635a720ee12
Taras Prokofiev <taras.prokofiev@devit-team.com>
2024-10-28 14:39:49 +0200

    worker example

---
+++ source/workers/syncOrders/stages/1-waitForFinish.ts
@@ -0,0 +1,5 @@
+import type {SyncOrdersTask} from '@prisma/client';
+
+export const waitForFinish = async (task: SyncOrdersTask) => {
+
+};
---
+++ source/workers/syncOrders/stages/2-downloadResult.ts
@@ -0,0 +1,5 @@
+import type {SyncOrdersTask} from '@prisma/client';
+
+export const downloadResult = async (task: SyncOrdersTask) => {
+
+};
